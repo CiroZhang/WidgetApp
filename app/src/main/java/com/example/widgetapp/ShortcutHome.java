0c6b0112276c96ca622012cf7c8aa59f8e90ab93
JasonLiang5129 <94155740+JasonLiang5129@users.noreply.github.com>
2022-04-12 21:52:04 -0700

    started xml changes on app

---
+++ app/src/main/java/com/example/widgetapp/ShortcutHome.java
@@ -0,0 +1,14 @@
+package com.example.widgetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShortcutHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shortcut_home);
+    }
+}
