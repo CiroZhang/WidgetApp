ae94c4c62ada874334c6980e5d863edd8d953612
JasonLiang5129 <94155740+JasonLiang5129@users.noreply.github.com>
2022-04-16 21:45:37 -0700

    more xml changes

--- app/src/main/java/com/example/widgetapp/SelectApps.java
+++ app/src/main/java/com/example/widgetapp/SelectApps.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CreateShortcut extends AppCompatActivity {
+public class SelectApps extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.create_shortcut);
+        setContentView(R.layout.select_apps);
     }
 }
